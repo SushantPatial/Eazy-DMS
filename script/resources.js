08e13d61e1d16a65b6a0ca40d25810cd1c6c219b
Sushant Patial <sushantpatial18@gmai.com>
2021-12-15 13:29:25 +0530

    hey

--- script/resources.js
+++ script/resources.js
@@ -5,23 +5,18 @@ function sendmail(){
   var Body='Hello';
 
   Email.send({
-    SecureToken:"b8344be3-0b41-4854-ac1b-1f3e5927e97c",
+    SecureToken:"d31d5b48-c70e-4f1e-9880-9f8c4425413b",
 				
     To: 'sushantpatial18@gmail.com',
     From: "patial.sushant12345@gmail.com",
     Subject: "New message on contact from",
-    Body: Body
+    Body : "And this is the body",
+    Attachments : [
+    {
+      name : "smtpjs.png",
+      path : "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
+    }]
   }).then(
-    message =>{
-      if(message=='OK'){
-      alert('Your mail has been send. Thank you for connecting.');
-      }
-      else{
-        console.error (message);
-        alert('There is error at sending message. ')
-        
-      }
-
-    }
+    message => alert(message)
   );
 }
