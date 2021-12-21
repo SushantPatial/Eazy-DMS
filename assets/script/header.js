f82ce88ba10c900eb7e7ca2a5c00d4f0ad156910
Sushant Patial <sushantpatial18@gmai.com>
2021-12-21 11:48:51 +0530

    interesting

--- assets/script/header.js
+++ assets/script/header.js
@@ -30,3 +30,20 @@ $(window).on("load",function() {
   $(".navbar").fadeIn("slow");
 });
 
+
+$('.industries').on('click', function() {
+  $('.nav-dropdown-content').toggle();
+})
+$(document).click(function(event) {
+  if (!$(event.target).hasClass('industries')) {
+    $('.nav-dropdown-content').css('display', 'none');
+  }
+});
+$(document).on('click', function() {
+  if ($('.nav-dropdown-content').attr('style') == 'display: block;') {
+    $('.fa-caret-down').addClass('flip');
+  }
+  else {
+    $('.fa-caret-down').removeClass('flip');
+  }
+})
