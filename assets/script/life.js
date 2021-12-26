615a2301cd5b1fc971ee76ba6229df15d406b8d3
Sushant Patial <sushantpatial18@gmai.com>
2021-12-27 01:25:51 +0530

    bruh when

--- assets/script/life.js
+++ assets/script/life.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function() {
-  
-  let $grid = $('.portfolio-container');
-
-  $grid.isotope({
-    itemSelector: '.portfolio-item',
-    filter: '.filter-awards',
-    layoutMode: 'masonry'
+  let $grid = $('.portfolio-container').imagesLoaded( function() {
+    $grid.isotope({
+      itemSelector : '.portfolio-item', 
+      filter: '.filter-awards',
+      layoutMode : 'masonry',
+      percentPosition: true
+    });
   });
     
   $('#portfolio-flters').on('click', 'li', function() {
@@ -46,18 +46,92 @@ const portfolioLightbox9 = GLightbox({
   selector: '.rishikesh-lightbox'
 });
 
-
-
-$('#year-20-21-toggler').on('click', function() {
-  $('#year-20-21').toggle();
+$(window).on('load', function() {
   $('.portfolio-container').isotope('reloadItems').isotope();
 })
 
+$('#year-20-21-toggler').on('click', function() {
+  $('#year-20-21').toggle();
+  $('#year-18-19').hide();
+  $('#year-17-18').hide();
+  $('.portfolio-container').isotope('reloadItems').isotope();
+})
 $('#year-18-19-toggler').on('click', function() {
   $('#year-18-19').toggle();
+  $('#year-20-21').hide();
+  $('#year-17-18').hide();
+  $('#year-18-19').addClass('window-resize');
   $('.portfolio-container').isotope('reloadItems').isotope();
 })
 $('#year-17-18-toggler').on('click', function() {
   $('#year-17-18').toggle();
+  $('#year-18-19').hide();
+  $('#year-20-21').hide();
+  $('#year-17-18').addClass('window-resize');
   $('.portfolio-container').isotope('reloadItems').isotope();
 })
+
+$('#diwali-19-toggler').on('click', function() {
+  $('#diwali-19').toggle();
+  $('#holi-19').hide();
+  $('#holi-19').addClass('window-resize');
+  $('.portfolio-container').isotope('reloadItems').isotope();
+
+})
+$('#holi-19-toggler').on('click', function() {
+  $('#holi-19').toggle();
+  $('#diwali-19').hide();
+  $('#holi-19').addClass('window-resize');
+  $('.portfolio-container').isotope('reloadItems').isotope();
+})
+$("#holi-19-toggler").one("click", function () {
+  setTimeout(function() {
+    $('#holi-19').removeClass('window-resize');
+  }, 500)
+});
+
+$('#hanuwantiya-toggler').on('click', function() {
+  $('#hanuwantiya').toggle();
+  $('#fortune-orange').hide();
+  $('#rishikesh').hide();
+  $('#golden-tulip').hide();
+  $('.portfolio-container').isotope('reloadItems').isotope();
+})
+$('#fortune-orange-toggler').on('click', function() {
+  $('#fortune-orange').toggle();
+  $('#hanuwantiya').hide();
+  $('#rishikesh').hide();
+  $('#golden-tulip').hide();
+  $('#fortune-orange').addClass('window-resize');
+  $('.portfolio-container').isotope('reloadItems').isotope();
+})
+$('#rishikesh-toggler').on('click', function() {
+  $('#rishikesh').toggle();
+  $('#fortune-orange').hide();
+  $('#hanuwantiya').hide();
+  $('#golden-tulip').hide();
+  $('#rishikesh').addClass('window-resize');
+  $('.portfolio-container').isotope('reloadItems').isotope();
+})
+$('#golden-tulip-toggler').on('click', function() {
+  $('#golden-tulip').toggle();
+  $('#fortune-orange').hide();
+  $('#rishikesh').hide();
+  $('#hanuwantiya').hide();
+  $('#golden-tulip').addClass('window-resize');
+  $('.portfolio-container').isotope('reloadItems').isotope();
+})
+
+
+$(window).resize(function() {
+  $('.masonry').addClass('window-resize');
+  setTimeout(function() {
+    $('.masonry').removeClass('window-resize');
+  }, 500)
+});
+
+$('.masonry .fa-times').on('click', function() {
+  $('.masonry').hide();
+  $('.portfolio-container').isotope('reloadItems').isotope();
+})
+
