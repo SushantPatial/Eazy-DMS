b4c00650c19043791f8dae8fb2631748ef84e2b8
Sushant Patial <sushantpatial18@gmai.com>
2022-01-08 20:26:43 +0530

    added lightbox

--- assets/script/clients.js
+++ assets/script/clients.js
@@ -1,4 +1,3 @@
-
 if ($(window).width() < 576) {
   $(".customers-text-1").attr("data-aos","fade-up");
   $(".customers-images-1").attr("data-aos","fade-up");
@@ -11,36 +10,6 @@ else {
   $(".customers-text-2").attr("data-aos","fade-left");
   $(".customers-images-2").attr("data-aos","fade-right");
 }
-
-
-// Video Swiper
-var mySwiper = new Swiper(".swiper-container-videos", {
-  loop: false,
-  slidesPerView: "auto",
-  spaceBetween: 15,
-  centeredSlides: false,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-  navigation: {
-    nextEl: ".swiper-custom-next",
-    prevEl: ".swiper-custom-prev"
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true
-  }
-});
-
-/* change active class when click */
-$(".swiper-container-videos .swiper-wrapper .swiper-slide a").click(function() {
-  $(this).closest(".swiper-slide").addClass("selected").siblings().removeClass("selected");
-  mySwiper.slideTo(mySwiper.clickedIndex);
-});
-
-$(".swiper-container-videos .swiper-slide").first().addClass("selected");
 
 
 $('#moreless-button1').click(function() {
@@ -74,17 +43,33 @@ $('#moreless-button3').click(function() {
 });
 
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+
+$('.slick-carousel').slick({
+  infinite: true,
+  slidesToShow: 3, // Shows a three slides at a time
+  slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+  prevArrow:"<button type='button' class='slick-nav pull-left'><i class='far fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-nav pull-right'><i class='far fa-angle-right' aria-hidden='true'></i></button>",
+  dots: true,
+
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
 
-
-
-  
+$('.slick-carousel').slickLightbox({
+  itemSelector: '.slick-item a',
+  navigateByKeyboard: true,
+});
 
